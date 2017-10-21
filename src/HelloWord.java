66344659fd368040582d8a56a0c48a3628292632
lemin2601 <lemin2601@gmail.com>
2017-10-21 16:20:47 +0700

    Update HelloWord.java
    edit server

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -5,7 +5,7 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        System.out.println("BCD");
+        System.out.println("server edit");
 
         System.out.println("abc");
         System.out.println("dce");
